b8748a82a1ff3e172b6dac38f7bbd416e764fb3c
Sandeep Tanjore <xyz@gmail.com>
2024-02-01 18:51:30 -0500

    Solutions for exercises 42 and 43

---
+++ Language Basics/05 Boolean Exercises/exercise42.js
@@ -0,0 +1,8 @@
+/**
+ * Exercise 42 - Say it's true
+ * Store the boolean value of true in a variable called myBoolean and display the
+ * contents of the variable to the console.
+ */
+
+let myBoolean = true;
+console.log(myBoolean);
---
+++ Language Basics/05 Boolean Exercises/exercise43.js
@@ -0,0 +1,8 @@
+/**
+ * Exercise 43 - Say it isn't true
+*  Store the boolean value of true in a variable called myBoolean and display the
+ * inverse value (false) of the variable when you display it to the console.
+ */
+
+let myBoolean = true;
+console.log(!myBoolean);
